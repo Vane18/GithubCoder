c5973abb327ac27cde366d55b5b43b2f1705ad71
Vane <vanesahabbo07@gmail.com>
2022-05-02 00:29:18 -0300

    Final_Clase_19

---
+++ AppCoder/urls.py
@@ -0,0 +1,12 @@
+import imp
+from django.urls import path
+from .views import *
+
+urlpatterns = [
+    path('',inicio),
+    path('profesores/',profesores),
+    path('estudiantes/',estudiantes),
+    path('cursos/',cursos),
+    path('entregables/',entregables),
+    path('creaCurso/',curso),
+]
--- AppCoder/views.py
+++ AppCoder/views.py
@@ -11,3 +11,17 @@ def curso(self):
     texto=f"------>Curso: {curso.nombre} Comision: {curso.comision}"
 
     return HttpResponse(texto)
+def inicio(request):
+    return render(request,'AppCoder/inicio.html')
+
+def profesores(request):
+    return HttpResponse("Esta es la pagina de profesores")
+
+def estudiantes(request):
+    return HttpResponse("Esta es la pagina de estudiantes")
+
+def cursos(request):
+    return HttpResponse("Esta es la pagina de cursos")
+
+def entregables(request):
+    return HttpResponse("Esta es la pagina de entregables")
